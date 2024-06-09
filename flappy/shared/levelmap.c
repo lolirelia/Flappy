6ffcde56ee13d295a84b02cc085cd2cf0b37afff
Akali <Akali@Macbook.pro>
2024-06-09 18:15:45 -0400

    init

---
+++ flappy/shared/levelmap.c
@@ -0,0 +1,30 @@
+#include "levelmap.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+static struct MapInstance g_levelmap;
+void LoadLevel() {
+    FILE* fMap = fopen("resources/level.map", "rb");
+    assert(fMap != NULL);
+    fseek(fMap, 0, SEEK_END);
+    uint32_t collisioncount = ftell(fMap);
+    fseek(fMap, 0, SEEK_SET);
+    collisioncount /= sizeof(struct Rectangle);
+    assert(collisioncount != 0);
+    g_levelmap.collisions = calloc(collisioncount, sizeof(struct Rectangle));
+    g_levelmap.collisioncount = collisioncount;
+    struct Rectangle index;
+    uint32_t n = 0;
+    while (fread(&index, sizeof(struct Rectangle), 1, fMap) == 1) {
+        g_levelmap.collisions[n++] = index;
+    }
+    assert(n == collisioncount);
+    fclose(fMap);
+}
+
+void DestroyMap() {
+    free(g_levelmap.collisions);
+    g_levelmap.collisions = 0;
+    g_levelmap.collisioncount = 0;
+}
---
+++ flappy/shared/netinterface.c
@@ -0,0 +1,171 @@
+
+#include "netinterface.h"
+static char* g_pool = NULL;
+#define kGlobalPoolSize 65536
+static int IsValidSocket(NetInterface* ni) {
+#ifdef NETINTERFACE_USING_WINDOWS
+    if (ni->m_niPeer == INVALID_SOCKET) return 0;
+#else
+    if (ni->m_niPeer == -1) return 0;
+#endif
+    return 1;
+}
+void NetSleep(uint32_t u32Miliseconds) {
+#ifdef NETINTERFACE_USING_WINDOWS
+    Sleep(u32Miliseconds);
+#else
+    u32Miliseconds *= 1000;
+    usleep(u32Miliseconds);
+#endif
+}
+static int NonBlock(NetInterface* ni) {
+#ifdef NETINTERFACE_USING_WINDOWS
+    int timeout = 1;
+    if (setsockopt(ni->m_niPeer, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout,
+                   sizeof(int)) != 0) {
+        DestroySocket(ni);
+        return 0;
+    }
+#else
+    struct timeval tv;
+    tv.tv_sec = 0;
+    tv.tv_usec = 1000;
+    if (setsockopt(ni->m_niPeer, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) !=
+        0) {
+        DestroySocket(ni);
+        return 0;
+    }
+#endif
+    return 1;
+}
+NetInterface* MakeNetInterface() {
+    NetInterface* ni = malloc(sizeof(NetInterface));
+    assert(ni != NULL);
+    g_pool = malloc(kGlobalPoolSize);
+    assert(g_pool != NULL);
+
+#ifdef NETINTERFACE_USING_WINDOWS
+    WSAData data = {0};
+    WSAStartup(MAKEWORD(2, 2), &data);
+#endif
+    return ni;
+}
+void DestroyNetInterface(NetInterface* ni) {
+    assert(IsValidSocket(ni) == 0);
+
+#ifdef NETINTERFACE_USING_WINDOWS
+    WSACleanup();
+#endif
+    free(ni);
+}
+
+int MakeSocket(NetInterface* ni) {
+    ni->m_niPeer = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    if (!IsValidSocket(ni)) return 0;
+
+    int enable = 1;
+    if (setsockopt(ni->m_niPeer, SOL_SOCKET, SO_REUSEADDR, (char*)&enable,
+                   sizeof(int)) != 0) {
+        DestroySocket(ni);
+        return 0;
+    }
+    NonBlock(ni);
+    return 1;
+}
+#include <stdio.h>
+int MakeSocketBind(NetInterface* ni) {
+    NISockAddrIn niSelfAddress = {0};
+    niSelfAddress.sin_family = AF_INET;
+    niSelfAddress.sin_port = htons(23456);
+    niSelfAddress.sin_addr.s_addr = INADDR_ANY;
+    if (!MakeSocket(ni)) {
+        return 0;
+    }
+
+    if (bind(ni->m_niPeer, (NISockAddr*)&niSelfAddress, sizeof(NISockAddrIn)) !=
+        0) {
+        DestroySocket(ni);
+        return 0;
+    }
+    return 1;
+}
+void Poll(NetInterface* ni, NiTransferCallback niCallback) {
+    assert(IsValidSocket(ni));
+
+    char msgbuffer[128];
+    memset(msgbuffer, 0, sizeof(msgbuffer));
+    NISockAddrIn niAddr = {0};
+    NISockAddrSize niUSizeOfAddr = sizeof(NISockAddrIn);
+
+    NITransferSize niTransferSize =
+        recvfrom(ni->m_niPeer, msgbuffer, sizeof(msgbuffer), 0,
+                 (NISockAddr*)&niAddr, &niUSizeOfAddr);
+
+    if (!IsErrorNoBlock(ni)) {
+        printf("%s\n", msgbuffer);
+    }
+}
+void SendPacket(NetInterface* ni, NetInterfaceAddr* niAddr, char* msgbuffer,
+                uint32_t size) {
+    assert(IsValidSocket(ni));
+    NITransferSize niTransferSize =
+        sendto(ni->m_niPeer, msgbuffer, size, 0,
+               (NISockAddr*)&niAddr->m_niRemoteAddr, niAddr->m_niUSizeOfAddr);
+    if (niTransferSize < 0) {
+        if (!IsErrorNoBlock(ni)) {
+        }
+    }
+}
+
+NetInterfaceAddr* MakeRemoteAddress(const char* cHostName, uint16_t u16Port) {
+    NetInterfaceAddr* niAddr = malloc(sizeof(NetInterfaceAddr));
+    if (niAddr != NULL) {
+#ifdef NETINTERFACE_USING_WINDOWS
+        InetPtonA(AF_INET, cHostName, &niAddr.m_niRemoteAddr.sin_addr);
+#else
+        inet_aton(cHostName, &niAddr->m_niRemoteAddr.sin_addr);
+#endif
+        niAddr->m_niRemoteAddr.sin_port = htons(u16Port);
+        niAddr->m_niRemoteAddr.sin_family = AF_INET;
+        niAddr->m_niUSizeOfAddr = sizeof(NetInterfaceAddr);
+    }
+    return niAddr;
+}
+void DestroyNetInterfaceAddr(NetInterfaceAddr* niAddr) {
+    assert(niAddr != NULL);
+    free(niAddr);
+}
+
+uint32_t GetError(NetInterface* ni) {
+#ifdef NETINTERFACE_USING_WINDOWS
+    return WSAGetLastError();
+#else
+    return errno;
+#endif
+}
+int IsErrorNoBlock(NetInterface* ni) {
+    assert(ni != NULL);
+    assert(IsValidSocket(ni));
+    int error = GetError(ni);
+#ifdef NETINTERFACE_USING_WINDOWS
+    if (error == WSAEWOULDBLOCK) {
+        return 1;
+    }
+#else
+    if (error == EWOULDBLOCK) {
+        return 1;
+    }
+#endif
+    return 0;
+}
+void DestroySocket(NetInterface* ni) {
+    assert(IsValidSocket(ni));
+#ifdef NETINTERFACE_USING_WINDOWS
+    closesocket(ni->m_niPeer);
+    ni->m_niPeer = INVALID_SCKET;
+#else
+    close(ni->m_niPeer);
+    ni->m_niPeer = -1;
+#endif
+}
