a762e3e7a8345f683ec48dbc23c5f0141bacd003
Akali <Akali@Macbook.pro>
2024-06-09 23:34:48 -0400

    i think it's best if I serialize

--- flappy/client/client.c
+++ flappy/client/client.c
@@ -1,13 +1,28 @@
+
+
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <uv.h>
 
 #include "gameshared.h"
 #include "levelmap.h"
+
+uv_udp_send_t send_req;
+uint32_t myid = 0;
+uint32_t tick = 0;
+uint32_t serverbasetick = 0;
+uint32_t starttick = 0;
 static void on_recv(uv_udp_t* handle, ssize_t nread, const uv_buf_t* rcvbuf,
                     const struct sockaddr* addr, unsigned flags) {
     if (nread > 0) {
-        printf("%lu\n", nread);
-        printf("%s", rcvbuf->base);
+        if(nread == sizeof(uint8_t)){
+			uint32_t packet = 0;
+			memcpy(&packet,rcvbuf->base,sizeof(packet));
+			myid = packet;
+			starttick = tick;
+			serverbasetick = 
+		}
     }
 
     free(rcvbuf->base);
@@ -17,7 +32,6 @@ static void on_alloc(uv_handle_t* client, size_t suggested_size,
                      uv_buf_t* buf) {
     buf->base = malloc(suggested_size);
     buf->len = suggested_size;
-
 }
 
 int main() {
@@ -28,11 +42,17 @@ int main() {
     assert(uv_ip4_addr("127.0.0.1", 23456, (struct sockaddr_in*)&host) == 0);
     assert(uv_udp_recv_start(&client, on_alloc, on_recv) == 0);
 
+    // join the game
+    uv_udp_send_t send_req;
+    uint16_t packet = 0xCAFE;
+    uv_buf_t buffer = uv_buf_init((char*)&packet, sizeof(packet));
+    uv_udp_send(&send_req, &client, &buffer, 1, &host, NULL);
+
     SetTraceLogLevel(LOG_NONE);
     SetTargetFPS(240);
     InitWindow(960, 540, "Flappy");
     double accumulator = 0.0;
-    uint32_t tick;
+    
 
     while (!WindowShouldClose()) {
         accumulator += GetFrameTime();
@@ -40,10 +60,17 @@ int main() {
             accumulator -= kTimestep;
             ++tick;
         }
-        uv_udp_send_t send_req;
-        uint16_t packet = 0xCAFE;
-        uv_buf_t buffer = uv_buf_init((char*)&packet, sizeof(uint16_t));
-        uv_udp_send(&send_req, &client, &buffer, 1, &host, NULL);
+        if (IsMouseButtonPressed(0)) {
+            
+            uint8_t packet = 1;
+            uv_buf_t buffer = uv_buf_init((char*)&packet, sizeof(packet));
+            uv_udp_send(&send_req, &client, &buffer, 1, &host, NULL);
+        } else if (IsMouseButtonReleased(0)) {
+            uint8_t packet = 0;
+            uv_buf_t buffer = uv_buf_init((char*)&packet, sizeof(packet));
+            uv_udp_send(&send_req, &client, &buffer, 1, &host, NULL);
+        }
+
         BeginDrawing();
         ClearBackground(BLACK);
         DrawFPS(100, 100);
