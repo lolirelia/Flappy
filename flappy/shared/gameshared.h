670036eb5fb2e133886d930b0ec36f55472de1f8
irelia <irelia@macbook.pro>
2024-06-12 13:17:48 -0400

    Server is crashing when sending a state packet..

--- flappy/shared/gameshared.h
+++ flappy/shared/gameshared.h
@@ -5,26 +5,54 @@
 #include <raymath.h>
 #include <stdint.h>
 #define kTimestep (1 / 60.0)
-#define kMaxNumberOfPlayers (1)
+#define kMaxNumberOfPlayers (2)
 #define kRenderDelayTicks (6)
 #define kGetPtr(x) (&x)
 #define kGetAddr(x) ((struct sockaddr_in*)kGetPtr(x))
 #define kGetAddrPtr(x) ((struct sockaddr_in*)x)
+typedef uint64_t FlappyPacket;
+enum EFlappyPacketId {
+    kEFlappyPacketIdNone = 0,
+    kEFlappyPacketIdInput,
+    kEFlappyPacketIdPlayerState,
+    kEFlappyPacketIdPlayerId,
+    kEFlappyPacketInsertPlayer,
+    kEFlappyPacketIdMex,
+};
+#define kPackInsertPlayer(x) (x = (kEFlappyPacketInsertPlayer))
+
+#define kPackPlayerId(x, y, z)                      \
+    (x = (((uint64_t)y << 48) | (uint64_t)z << 16 | \
+          (uint16_t)kEFlappyPacketIdPlayerId))
+
+#define kPackPlayerInput(x, y) \
+    (x = (((uint64_t)y << 48) | (uint16_t)kEFlappyPacketIdInput))
+
+#define kPackPlayerPosition(x, y) \
+    (memcpy(kGetPtr(x), kGetPtr(y), sizeof(uint64_t)))
+
+#define kGetPlayerId(x) ( (x >> 48) & 0xFFFF )
+#define kGetInput(x) ( (x >> 48) & 0xFFFF )
+#define kGetTick(x) ( (x >> 16) & 0xFFFFFFFF )
+#define kGetPacketId(x) ( x & 0xFFFF ) 
+struct PlayerState {
+    uint64_t x;
+    uint64_t y;
+};
+struct GamestateClient {
+    struct PlayerState players[kMaxNumberOfPlayers];
+};
 struct PlayerClient {
     struct Vector2 position;
-    uint8_t id;
+    uint16_t id;
 };
 struct PlayerServerside {
     struct PlayerClient player;
     Vector2 velocity;
     uint8_t isflapping;
-    struct sockaddr addr;
-};
-
-;
-struct GamestateClient {
-    struct PlayerClient players[kMaxNumberOfPlayers];
-    uint32_t tick;
+    uint32_t ipv4;
+    uint16_t port;
+    struct sockaddr_in addrin;
 };
 
 struct GamestateServerside {
@@ -32,14 +60,6 @@ struct GamestateServerside {
     uint32_t playercount;
     uint32_t tick;
     uint8_t gamestarted;
-};
-
-enum NetInterfacePacketId {
-    kNetPacketIdNone = 0,
-    kNetPacketIdInput,
-    kNetPacketIdGamestate,
-    kNetPacketIdPlayerId,
-    kNetPacketIdMex,
 };
 
 #endif
